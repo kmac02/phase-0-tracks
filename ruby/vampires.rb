ccf94a048ad708d5d31a9804b1896fa05a44bd48
kristinmac <kristin.macdonough@gmail.com>
2017-07-21 09:59:51 -0500

    Add trick questions for data collection

---
+++ ruby/vampires.rb
@@ -0,0 +1,6 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What is your birthday?"
+birthday = get.chomp
+puts "Should the cafeteria order extra garlic bread for you?"
+puts "Would you like to enroll in the company's health insurance?"
