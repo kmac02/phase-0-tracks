cdc5359272706c676f65c700d4fb5e8bdb9c3a77
kristinmac <kristin.macdonough@gmail.com>
2017-07-21 13:01:49 -0500

    Add first attempts at detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,19 +1,34 @@
+=begin
+vampire_name = true
+correct_age = true
+likes_garlic = true
+wants_insurance = true
+
+
+=end
+
+
+
 puts "What is your name?"
 name = gets.chomp
+if name == "Drake Cula" || name == "Tu Fang"
+  vampire_name = true
+else
+  vampire_name = false
+end
 
 puts "How old are you?"
   age = gets.chomp
 puts "What year were you born?"
   birthday_year = gets.chomp
 
-=begin
+
 if age.to_i == (2017 - birthday_year.to_i)
-  age_right = true
+  correct_age = true
   # if the age is correct, not a vampire
 else
-  age_right = false
+   correct_age = false
 end
-=end
 
 
 puts "Should the cafeteria order extra garlic bread for you?"
@@ -33,11 +48,41 @@ else
   wants_insurance = false
 end
 
-
-if age_right && (likes_garlic || wants_insurance)
-  puts "Probably not a vampire"
-elsif !age_right && !likes_garlic
-  puts "Probably a vampire"
-else
-  puts "results inconclusive"
+case
+when (correct_age && (likes_garlic || wants_insurance))
+  if true && !vampire_name
+    puts "Probably not a vampire"
+  else true && vampire_name
+    puts "Definitely a vampire"
+  end
 end
+
+#If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+case
+when !(!correct_age && !(likes_garlic || wants_insurance))
+  if true
+    puts "Probably a vampire"
+  else false
+  end
+end
+  # !(false and (false or both false))
+
+
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+case
+when !(!correct_age && !likes_garlic && !wants_insurance)
+  # !(false and false and false)
+  if true
+    puts "Almost certainly a vampire"
+  end
+else
+  "Results inconclusive"
+end
+# I'm really confused by the two middle conditions, they are almost the same thing. the more i worked on them, the more messy they became.
+
+
+
+
+
+
+
