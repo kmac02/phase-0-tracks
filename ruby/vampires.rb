4508f62dd1e6355c8227d4928b4aee9797c18909
kristinmac <kristin.macdonough@gmail.com>
2017-07-22 18:15:13 -0500

    Copy results from repl, clean up if/else

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,21 +1,19 @@
 puts "How many applications to process?"
 apps_to_process = gets.chomp.to_i
 
-until apps_to_process == 0
+until apps_to_process <= 0
   puts "What is your name?"
   name = gets.chomp
   if name == "Drake Cula" || name == "Tu Fang"
     vampire_name = true
   else
-    vampire_name = false
+    not_vampire_name = true
   end
 
   puts "How old are you?"
     age = gets.chomp
   puts "What year were you born?"
     birthday_year = gets.chomp
-
-
   if age.to_i == (2017 - birthday_year.to_i)
     correct_age = true
     # if the age is correct, not a vampire
@@ -42,40 +40,27 @@ until apps_to_process == 0
   end
 
 
-
-  case
-  when correct_age && (likes_garlic || wants_insurance)
-    if true && !vampire_name
-      puts "Probably not a vampire"
-    else true && vampire_name
-      puts "Definitely a vampire"
-    end
+if correct_age && (likes_garlic || wants_insurance) && not_vampire_name
+  puts "Probably not a vampire"
+elsif incorrect_age && doesnt_like_garlic && doesnt_want_insurance
+  puts "Almost certainly a vampire" # i think this elsif is supposed to go to the bottom
+elsif incorrect_age && doesnt_like_garlic && wants_insurance && not_vampire_name
+  puts "Probably a vampire"
+elsif incorrect_age && likes_garlic && doesnt_want_insurance && not_vampire_name
+  puts "Probably a vampire"
+elsif correct_age && likes_garlic && wants_insurance
+  if vampire_name == true
+    puts "Definitely a vampire"
   end
+else
+  #incorrect age but likes garlic and wants insurance, or correct age but doesn't like garlic or want insurance
+  puts "results inconclusive"
+end
 
-  #If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-  case
-  when incorrect_age && (doesnt_like_garlic || doesnt_want_insurance)
-    if true
-      puts "Probably a vampire"
-    else false
-    end
-  end
-    # !(false and (false or both false))
-
-  # If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
-  case
-  when incorrect_age && doesnt_like_garlic && doesnt_want_insurance
-    # (false and false and false)
-    if true
-      puts "Almost certainly a vampire"
-    end
-  else false
-    puts "Results inconclusive"
-  end
-  # I'm really confused by the two middle conditions, they are almost the same thing, and the more i worked on them, the messier they became. And when i ran the program, i could only return "Almost certainly...' if I also returned 'Probably a vampire'. I'm having trouble avoiding repetition here."
+  # i suspect there is a cleaner way to do the above. This is the first strategy i tried where i was able to return only one survey response (versus 2 or 3) no matter the input. (i was pretty excited). I think this would be cleaner using a bang operator but the assignment said it wasn't necessary for this challenge. I look forward to feedback!
 
   puts "Please name your allergies one at a time. Type 'done' when completed"
-  some_allergies = gets.chomp
+  some_allergies = gets.chomp #variable must have input before loop can begin.
     while some_allergies != "done"
       puts "what other allergies?"
       some_allergies = gets.chomp
@@ -87,9 +72,6 @@ until apps_to_process == 0
 
 apps_to_process -= 1
 end
-
-
-
 
 
 
