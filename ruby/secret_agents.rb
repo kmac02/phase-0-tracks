7f9b0fdb6ab29908e8f5ade218abebd08c6dd063
kristinmac <kristin.macdonough@gmail.com>
2017-07-22 17:00:41 -0500

    Add interface with driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -7,20 +7,19 @@ def encrypt(password) #names method, declares parameter
   index = 0
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   while index < password.length
-    p index_in_alpha =  alphabet.index(password[index]) #integer
+    index_in_alpha =  alphabet.index(password[index]) #integer
       if index_in_alpha == 25
         new_index = 0
       else
         new_index = index_in_alpha + 1
       end
-    p new_pass += alphabet[new_index]
-
+    new_pass += alphabet[new_index]
     index += 1
   end
   new_pass #return
 end
 
-encrypt("abc") #invoke the method
+encrypt("zed") #invoke the method
 
 
 def decrypt(password)
@@ -31,14 +30,50 @@ def decrypt(password)
 
   while index < password.length
     index_in_alpha =  alphabet.index(password[index]) #integer
-       p new_index = index_in_alpha - 1
+    new_index = index_in_alpha - 1
     new_pass += alphabet[new_index]
     index += 1
   end
   new_pass #return
 end
 
-decrypt("abc")
+decrypt("afe")
 
-decrypt(encrypt("abc"))
-# the encrypt method is called first and moves the index forward one, then the decrypt method is called on the encrypted text and moves the index back one.
+decrypt(encrypt("swordfish"))
+# the encrypt method is called first and moves the index forward one, then the decrypt method is called on the encrypted text and moves the index back one. They use the same index.
+
+# Driver code
+
+#Ask user if they want to encrypt or decrypt.
+#Create a variable for response (not encrypt or decrypt)
+#Capture response and apply appropriate method
+
+puts "Do you want to encrypt or decrypt your password?"
+encoding_type = gets.chomp
+
+
+#while encoding_type != "encrypt" || encoding_type != "decrypt"
+ #   puts "Please enter either encrypt or decrypt."
+  #  encoding_type = gets.chomp
+   if encoding_type == "encrypt"
+      puts "What is your password?"
+      password = gets.chomp
+      puts encrypt(password)
+      #break
+    else encoding_type == "decrypt"
+      puts "What is your password?"
+      password = gets.chomp
+      puts decrypt(password)
+      #break
+    end
+#end
+
+
+
+
+#Ask user for the password
+#Capture response and use selected method
+
+
+
+#Print results to screen
