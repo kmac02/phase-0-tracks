d9e6524a1007f15547455676f99fe9623bb0bf15
kristinmac <kristin.macdonough@gmail.com>
2017-07-23 10:03:28 -0500

    final edits, proper indents and spell check

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,16 +13,15 @@ until apps_to_process <= 0 #this can be == as well
   end
 
   puts "How old are you?"
-    age = gets.chomp
+  age = gets.chomp
   puts "What year were you born?"
-    birthday_year = gets.chomp
+  birthday_year = gets.chomp
   if age.to_i == (2017 - birthday_year.to_i)
     correct_age = true
     # if the age is correct, not a vampire
   else
      incorrect_age = true
   end
-
 
   puts "Should the cafeteria order extra garlic bread for you?"
   garlic_bread = gets.chomp
@@ -42,22 +41,22 @@ until apps_to_process <= 0 #this can be == as well
   end
 
 # DETECTION LOGIC
-if correct_age && (likes_garlic || wants_insurance) && not_vampire_name
-  puts "Probably not a vampire"
-elsif incorrect_age && doesnt_like_garlic && wants_insurance && not_vampire_name
-  puts "Probably a vampire"
-elsif incorrect_age && likes_garlic && doesnt_want_insurance && not_vampire_name
-  puts "Probably a vampire"
-elsif incorrect_age && doesnt_like_garlic && doesnt_want_insurance
-  puts "Almost certainly a vampire"
-elsif correct_age && likes_garlic && wants_insurance
-  if vampire_name == true
-    puts "Definitely a vampire"
+  if correct_age && (likes_garlic || wants_insurance) && not_vampire_name
+    puts "Probably not a vampire"
+  elsif incorrect_age && doesnt_like_garlic && wants_insurance && not_vampire_name
+    puts "Probably a vampire"
+  elsif incorrect_age && likes_garlic && doesnt_want_insurance && not_vampire_name
+    puts "Probably a vampire"
+  elsif incorrect_age && doesnt_like_garlic && doesnt_want_insurance
+    puts "Almost certainly a vampire"
+  elsif correct_age && likes_garlic && wants_insurance
+    if vampire_name == true
+      puts "Definitely a vampire"
+    end
+  else
+    #incorrect age but likes garlic and wants insurance, or correct age but doesn't like garlic or want insurance
+    puts "results inconclusive"
   end
-else
-  #incorrect age but likes garlic and wants insurance, or correct age but doesn't like garlic or want insurance
-  puts "results inconclusive"
-end
 
 # i suspect there is a cleaner way to do the above. This is the first strategy i tried where i was able to return only one survey response (versus 2 or 3) no matter the input. (i was pretty excited). I think this would be cleaner using a bang operator but the assignment said it wasn't necessary for this challenge. I look forward to feedback!
 
@@ -76,7 +75,7 @@ end
 
 apps_to_process -= 1
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-end # end of until apps to process loop
+end # end of until apps_to_process loop
 
 
 
