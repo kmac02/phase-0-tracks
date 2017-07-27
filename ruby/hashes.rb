735c3c76687b7ecc191bc36c81ecaa6055a09770
kristinmac <kristin.macdonough@gmail.com>
2017-07-27 15:00:18 -0500

    add questionnaire hash, add prompts for user input

---
+++ ruby/hashes.rb
@@ -0,0 +1,71 @@
+#Declare keys (with empty values) in questionnaire
+questionnaire = {
+  name: "",
+  age: "",
+  family_size: "", #integer
+  children_under_10: "", #boolean
+  decor_theme: "",
+  number_of_rooms: "", #integer
+  need_new_walls: "", #boolean
+  has_pets: "", #boolean
+  number_of_pets: ""
+}
+
+#Ask user to begin filling out questionnaire
+puts "Please begin entering client information."
+
+puts "Client's name:"
+name = gets.chomp
+questionnaire[:name] = name
+
+puts "Client's age:"
+age = gets.chomp
+questionnaire[:age] = age.to_i
+
+puts "How many people in the family?"
+family_size = gets.chomp
+questionnaire[:family_size] = family_size.to_i
+
+puts "Does the client have children under the age of 10?"
+kids_under_ten = gets.chomp
+  if kids_under_ten == "yes" || kids_under_ten == "y"
+    questionnaire[:children_under_10] = true
+  else
+    questionnaire[:children_under_10] = false
+  end
+
+puts "What decor theme?"
+decor = gets.chomp
+questionnaire[:decor_theme] = decor
+
+puts "How many rooms?"
+room_number = gets.chomp
+questionnaire[:number_of_rooms] = room_number.to_i
+
+puts "Does the client need new walls?"
+needs_walls = gets.chomp
+  if needs_walls == "yes" || needs_walls == "y"
+    questionnaire[:need_new_walls] = true
+  else
+    questionnaire[:need_new_walls] = false
+  end
+
+puts "Does the client have pets?"
+pets = gets.chomp
+  if pets == "yes" || pets == "y"
+    questionnaire[:has_pets] = true
+    puts "How many?"
+    pet_number = gets.chomp
+    questionnaire[:number_of_pets] = pet_number.to_i
+  else
+    questionnaire[:has_pets] = false
+    questionnaire[:has_pets] = nil
+  end
+
+
+
+questionnaire.each { |key, value| puts "Client info: #{key}: #{value}."}
+
+
+
+p questionnaire
