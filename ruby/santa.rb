74f32aa8daca6facea0bee2c57d684849a4a48d8
kristinmac <kristin.macdonough@gmail.com>
2017-08-01 22:18:13 -0500

    Add setter methods, test with driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,6 +5,8 @@ class Santa
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -15,9 +17,35 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
+  def celebrate_birthday (age)
+    @age = age.to_i + 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking[-1] = reindeer
+  end
+
+  def gender_set=(gender_ident)
+    @gender = gender_ident
+  end
 end
 
 #Driver Code
-santa = Santa.new
+santa = Santa.new("gender fluid", "latinx")
 santa.eat_milk_and_cookies("chocolate chip")
 santa.speak
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+#Test setter methods
+p santa.get_mad_at("Comet")
+p santa.gender_set = "male"
+p santa.celebrate_birthday(35)
