c259e69700654af2bae5781ad689b7eacf6dd4c3
kristinmac <kristin.macdonough@gmail.com>
2017-08-01 21:13:16 -0500

    add behaviors and characteristics to a class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+ANIMAL ------------------------
+Puppicans
+
+CHARACTERISTICS ----------------
+Ear length: varies
+Paw size: varies
+Playful: yes
+Hungry: yes
+Sleep hours required: 12
+
+
+BEHAVIOR -----------------------
+Paw
+Bark
+Chew
+Wags
+=end
