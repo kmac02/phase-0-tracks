234ed2c38dae569afbbff90201f08af57f06628d
kristinmac <kristin.macdonough@gmail.com>
2017-08-09 18:30:52 -0500

    Create arrays, add to arrays

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colors = [ "greenish", "reddish", "purplesque", "grey"];
+
+var horseNames = ["Kit-Kat", "Brandy", "Peter Pan", "Snowball"];
+
+colors.push("orange");
+
+horseNames.push("Pepper");
