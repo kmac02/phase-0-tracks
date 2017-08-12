51813c6d623eacf948e3bac743df0b2e8508a745
kristinmac <kristin.macdonough@gmail.com>
2017-08-12 12:25:15 -0500

    Begin release to generate random word

--- js/algos.js
+++ js/algos.js
@@ -18,14 +18,13 @@ function findLongWord (array) {
     return 0;
   } // end compare words function
   // create a new array of the sorted array
-  var orderedArray = (array.sort(compareWords))
+  var orderedArray = (array.sort(compareWords));
   // return the last (aka longest) string in the new array.
-  return (orderedArray.pop())
+  return (orderedArray.pop());
 
-} //end long word function
+} //end find long word function
 
-yourFunction({name: "Steven", age: 54}, {name: "Tamir", age: 54});
-yourFunction({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3});
+//-----------------------------------------------
 
 function comparePairs(kvPair1, kvPair2) {
 // turn the keys of each pair into an array
@@ -33,13 +32,76 @@ function comparePairs(kvPair1, kvPair2) {
 
 // compare each array for equality
 
- // for (var kvPair1, kvPair2
- //    return
- //  else if kvPair1 != kvPair2
- //    return
+  var theKeys = {kvPair1, kvPair2};
+  var keyArray = Object.keys(theKeys);
+  function compareKeys(oneKey, otherKey) {
+    for (var i = 0; i < keyArray.length; i++) {
+      if (oneKey == otherKey) {
+        return true;
+      }
+      else if (oneKey != otherKey) {
+        return false;
+      }
+    }// end loop
+
+  }// end compareKeys function
+
+  // var theValues = {kvPair1, kvPair2};
+  // var valueArray = Object.values(theValues);
+  //   function compareValues(oneValue, otherValue) {
+  //     for (var i = 0; i < keyArray.length; i++) {
+  //       if (oneValue === otherValue) {
+  //         return true;
+  //       }
+  //       else if (oneValue != otherValue) {
+  //         return false;
+  //       }
+  //     }// end loop
+
+  // }// end compareValues function
+
+  // determine if the return value for either the keys function or the values function is true.
+  function checkComparisons() {
+    var result = ''
+    if (compareValues === true || compareKeys === true) {
+      result = true;
+    }
+    else if (compareValues !== true || compareKeys !== true) {
+      result = false;
+    }
+    return result
+  }
+
+ } // end compare pairs function
+
+ // ---------------------------------------------------
+// Generate random test data
+// Write a function that takes an integer for length, and builds and returns an array of strings of the given length. So if we ran your function with an argument of 3, we would get an array of 3 random words back (the words don't have to be actual sensical English words -- "nnnnfph" totally counts). The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)
+// Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+
+function makeArray(integer) {
+  // create empty array
+  randomWords = []
+  alphabetArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  for (var i = 0; i < integer; i++) {
+    var randomNumber = Math.floor(Math.random() * 100) + 1; // turn #'s into letters?
+    var numberArray = randomNumber.toString.split("");
+    for (var a = 0; a < randomNumber; a++) {
+      newWord = ""
+      //match index of random numbers to index of letters in alphabet array
+      numberArray[a] = alphabetArray[a]
 
 
-} // end comparePairs
+    }
+    //feed into the randomWords array
+    randomWords.push(//thenewword)
+  } // end of loop
+  return randomWords
+} // end makeArray function
+
+
+
+
 
 // DRIVER CODE- for compareWords
 // verify words are in order
@@ -47,9 +109,11 @@ function comparePairs(kvPair1, kvPair2) {
 // print the last (aka longest) string in the array.
 //console.log(testArray.pop())
 
-console.log(findLongWord(testArray))
-console.log(findLongWord(titles))
+console.log(findLongWord(testArray));
+console.log(findLongWord(titles));
 
 
 
 console.log(comparePairs({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+
+console.log(makeArray(3));
