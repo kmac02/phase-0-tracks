c061ec32adf573b0065fbb43d3490a01de810caa
kristinmac <kristin.macdonough@gmail.com>
2017-08-12 08:51:23 -0500

    Create pseudocode

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+// Find the longest phrase in an array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase".
+
+// Create array of phrases.
+testArray(["long phrase","longest phrase","longer phrase"])
+// find the length of each string in the array
+
+// compare the lengths of each string
+// return the longest string
+
