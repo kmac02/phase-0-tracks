b28b550fbf154ed5b0baa3f44d917146bc0ce325
kristinmac <kristin.macdonough@gmail.com>
2017-08-12 09:37:07 -0500

    Add function to compare words

--- js/algos.js
+++ js/algos.js
@@ -1,9 +1,25 @@
-// Find the longest phrase in an array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase".
+// Create test array of strings
+var testArray = ["long phrase","longest phrase","longer phrase"]
+//Organize the strings by length
 
-// Create array of phrases.
-testArray(["long phrase","longest phrase","longer phrase"])
-// find the length of each string in the array
+function findLongWord (array) {
 
-// compare the lengths of each string
-// return the longest string
+function compareWords(oneWord, anotherWord) {
+  // if one word is longer than another word, send it to the back
+  if (oneWord.length < anotherWord.length)
+    return -1;
+  // if one word is shorter than another word, send it to the front
+  else if (oneWord.length > anotherWord.length)
+    return 1;
+  // otherwise leave it where it is.
+  return 0;
+
+ } // end compare
+
+} //end long word function
+// verify words are in order
+//console.log(testArray.sort(compareWords));
+// print the last (aka longest) string in the array.
+console.log(testArray.pop())
+
 
