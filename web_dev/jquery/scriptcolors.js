79f01a2727379bfe25eeb58a48d45099338eb45b
kristinmac <kristin.macdonough@gmail.com>
2017-08-22 20:32:04 -0500

    Practice fade in and out with jquery

--- web_dev/jquery/scriptcolors.js
+++ web_dev/jquery/scriptcolors.js
@@ -1,13 +1,43 @@
-// function fadeImage(event) {
-//   event.target.source = "blizzard.gif"
-// }
+
 var background = document.getElementsByTagName('body')[0];
 background.style.backgroundColor = '#000000';
 
 
 
 $(document).ready(function() {
-  $( "div:first" ).click(function() {
-    $( this).fadeTo( "slow", .33 );
+  $( "div#snow" ).mouseover(function() {
+    $( this).fadeOut("slow", function() {
+      $( this).hide();
+    })
   });
 });
+
+$(document).ready(function() {
+
+    $( "section" ).click(function() {
+    $( this).fadeIn("slow");
+  });
+});
+
+// $(document).ready(function() {
+//   if ( $ ("div#snow").is (":hidden") ) {
+//     $( "section" ).hide();
+//   } else {
+//     $( "section" ).show( "slow" );
+//   }
+
+// });
+
+
+// $( this ).mouseover(function() {
+      // $( this ).fadeIn
+// $(document).ready(function() {
+//  $("div").fadeOut(500, function() {
+//     $(this).attr("src", "fun_bars.gif");
+//     $(this).load(function () {
+//       $(this).fadeIn(500);
+//     });
+//   });
+// });
+
+// Sometimes it works, sometimes it doesn't. I don't give up but my time block is over.
