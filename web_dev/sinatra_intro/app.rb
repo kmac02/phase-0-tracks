9240239dad2f4aee68563cc3974923892bb9f362
kristinmac <kristin.macdonough@gmail.com>
2017-08-23 20:51:02 -0500

    Add routes to ruby file

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,39 +8,64 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} years old."
+# end
 
 # write a GET route with
 # route parameters
-get '/about/:person' do
-  person = params[:person]
-  "#{person} is a programmer, and #{person} is learning Sinatra."
-end
+# get '/' do
+#   person = params[:person]
+#   "#{person} is a programmer, and #{person} is learning Sinatra."
+# end
 
-get '/:person_1/loves/:person_2' do
-  "#{params[:person_1]} loves #{params[:person_2]}"
-end
+# get '/:person_1/loves/:person_2' do
+#   "#{params[:person_1]} loves #{params[:person_2]}"
+# end
 
-# write a GET route that retrieves
-# all student data
-get '/students' do
-  students = db.execute("SELECT * FROM students")
-  response = ""
-  students.each do |student|
-    response << "ID: #{student['id']}<br>"
-    response << "Name: #{student['name']}<br>"
-    response << "Age: #{student['age']}<br>"
-    response << "Campus: #{student['campus']}<br><br>"
-  end
-  response
-end
+# # write a GET route that retrieves
+# # all student data
+# get '/students' do
+#   students = db.execute("SELECT * FROM students")
+#   response = ""
+#   students.each do |student|
+#     response << "ID: #{student['id']}<br>"
+#     response << "Name: #{student['name']}<br>"
+#     response << "Age: #{student['age']}<br>"
+#     response << "Campus: #{student['campus']}<br><br>"
+#   end
+#   response
+# end
 
-# write a GET route that retrieves
-# a particular student
+# # write a GET route that retrieves
+# # a particular student
 
-get '/students/:id' do
-  student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
-  student.to_s
+# get '/students/:id' do
+#   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+#   student.to_s
+# end
+
+# # route that displays an address
+# get '/contact' do
+#   "555 Fifth Ave"
+# end
+
+# /great_job route that can take a person's name as a query parameter (not a route parameter)
+# and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+
+# get '/great_job/:name' do
+#   name = params[:name]
+#   if name
+#     "Good job, #{name}!"
+#   else
+#     "Good job!"
+#   end
+# end
+
+#A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+get '/:int01/plus/:int02' do
+  int01 = params[:int01]
+  int02 = params[:int02]
+  int_sum = int02.to_i + int01.to_i
+  int_sum.to_s
 end
