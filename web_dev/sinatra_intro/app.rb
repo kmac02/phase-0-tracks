43b2d38071424c5963de3423b8e77cef89ce42ee
kristinmac <kristin.macdonough@gmail.com>
2017-08-23 21:43:02 -0500

    Add database search in sinatra

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -63,9 +63,28 @@ db.results_as_hash = true
 # end
 
 #A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
-get '/:int01/plus/:int02' do
-  int01 = params[:int01]
-  int02 = params[:int02]
-  int_sum = int02.to_i + int01.to_i
-  int_sum.to_s
+# get '/:int01/plus/:int02' do
+#   int01 = params[:int01]
+#   int02 = params[:int02]
+#   int_sum = int02.to_i + int01.to_i
+#   int_sum.to_s
+# end
+
+# Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute.
+get '/students/' do
+  student = db.execute("SELECT name FROM students WHERE name LIKE ?", ['K%'])
+  student.to_s
 end
+
+# WORKS:
+ # students = db.execute("SELECT * FROM students")
+# students.to_s
+
+
+
+#RETURNS AN EMPTY ARRAY TO THE BROWSER
+# get '/students/:letter' do
+#   letter = params[:letter]
+#   student = db.execute("SELECT name FROM students WHERE name LIKE ?", ['[params[:letter]%'])
+#   student.to_s
+# end
